20d1907bc123510254dd9a6cc5615999c39fe0ae
lourduradjou <rajlourdu15@gmail.com>
2025-08-16 18:02:15 +0530

    chore: config changes

--- next.config.ts
+++ next.config.ts
@@ -1,8 +1,8 @@
-import type { NextConfig } from "next";
+import type { NextConfig } from 'next'
 /** @type {import('next').NextConfig} */
 
 const nextConfig: NextConfig = {
-  /* config options here */
-};
+    /* config options here */
+}
 
-export default nextConfig;
+export default nextConfig
